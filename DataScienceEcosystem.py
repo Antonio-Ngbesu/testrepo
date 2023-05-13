c8cd74e1e39ccfa16fb997bd0b31fe588c142e5c
Antonio Ngbesu <93670964+Antonio-Ngbesu@users.noreply.github.com>
2023-05-13 11:02:10 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+#     
+#     1. Python
+#     2. R
+#     3. Java
+#     4. Julia
+
+# Data Science libraries:
+#    * TensorFlow.
+#    * NumPy.
+#    * SciPy.
+#    * Pandas.
+#    * Matplotlib.
+#    * Keras.
+#    * SciKit-Learn.
+#    * PyTorch.
+
+# In[ ]:
+
+
+
+
